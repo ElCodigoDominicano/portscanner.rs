bf15d0ccd7539f13c37831cff5787ba07522d5c6
El Codigo Dominicano <el.codigo.dominicano@outlook.com>
2024-02-14 20:23:04 -0500

    Update main.rs

--- portscanner/src/main.rs
+++ portscanner/src/main.rs
@@ -1,7 +1,7 @@
 /// portscanner.rs, just another network information gathering tool
 /// written in rust, simple smalll and blazingly fast.. microseconds
 /// to seconds (< 2) for a complete TCP port scan of a system.
-///
+///  
 ///  NOT FINAL
 ///
 ///
@@ -19,6 +19,8 @@ use std::process::exit;
 use std::time::Instant;
 
 pub fn port_scanner() -> Box<Vec<u16>> {
+    os_enforce();
+
     let mut vec_of_open_ports:  Box<Vec<u16>> = Box::new(Vec::new());
 
     let mut socket: SocketAddr = SocketAddr::new(
@@ -26,17 +28,20 @@ pub fn port_scanner() -> Box<Vec<u16>> {
             Ipv4Addr::new(127, 0, 0, 1)), 1);
 
     for port in 2..65535 {
-        if OS == "linux" {
-            if let Ok(_stream) = TcpStream::connect(&socket) {
-                vec_of_open_ports.push(socket.port().to_owned());
-            }
-        } else {
-            println!("My apologies, but {:?} hasn't been implemented yet.", OS.to_string());
-            exit(0);
+        if let Ok(_stream) = TcpStream::connect(&socket) {
+            vec_of_open_ports.push(socket.port().to_owned());
         }
         socket.set_port(port);
     }
     vec_of_open_ports
+}
+
+fn os_enforce() {
+    if OS != "linux" {
+        eprintln!("The operating system {:?} for which this \
+        program is ran in, has yet been implemented.", OS.to_string())
+    }
+    exit(0);
 }
 
 pub fn timer() {
